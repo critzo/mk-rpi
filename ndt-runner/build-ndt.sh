cf0bfb640cec0316e72e879a4612d1d6b4499789
Chris Ritzo <critzo@opentechinstitute.org>
2017-07-26 11:59:48 -0400

    standardized build script path to pi user's home

--- ndt-runner/build-ndt.sh
+++ ndt-runner/build-ndt.sh
@@ -3,27 +3,27 @@ apt-get update
 apt-get install -y automake gcc make libssl-dev libjansson-dev python paris-traceroute screen
 
 # Build NDT and I2util
-cd ~/mlab-ndt
+cd /home/pi/mlab-ndt
 git clone --recursive https://github.com/ndt-project/ndt
-cd ndt/I2util && ./bootstrap.sh && ./configure && make && make install
-cd ~/mlab-ndt/ndt && ./bootstrap && ./configure && make
-cd ~
+cd /home/pi/mlab-ndt/ndt/I2util && ./bootstrap.sh && ./configure && make && make install
+cd /home/pi/mlab-ndt/ndt && ./bootstrap && ./configure && make
+cd /home/pi/
 
 # Move the NDT Binary (web100clt) to a common location and make a link to it.
 # This enables you to run NDT like any other program on your Pi.
-mv ndt/src/web100clt /opt/web100clt
+mv /home/pi/mlab-ndt/ndt/src/web100clt /opt/web100clt
 ln -s /opt/web100clt /usr/local/bin/web100clt
 
 # Install required python libraries
-cd ~/mlab-ndt/ndt-runner
+cd /home/pi/mlab-ndt/ndt-runner
 pip install pytz tzlocal
 
 # Setup data directories and files
-cd ~/mlab-ndt
+cd /home/pi/mlab-ndt
 mkdir dashboard/data
 touch dashboard/data/ndt-history.csv
 
 # Prepare the Paris Traceroute dataset.
-cd pt-analysis
+cd /home/pi/mlab-ndt/pt-analysis
 python main.py
-cp *.csv ~/mlab-ndt/dashboard/data/
+cp *.csv /home/pi/mlab-ndt/dashboard/data/
