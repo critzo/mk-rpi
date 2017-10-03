bb8dbdd18f7889599597343018d1694d5b721a81
Chris Ritzo <critzo@opentechinstitute.org>
2017-10-03 11:57:40 -0400

    adding opts to setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
+while getopts u:f: option
+do
+ case "${option}"
+ in
+ u) USER=${OPTARG};;
+ f) =${OPTARG};;
+ esac
+done
+
 # Get build tools and required packages
 apt-get update
-apt-get install -y dh-autoreconf autoconf automake libtool gcc make libssl-dev libevent-dev libgeoip-dev python python-pip paris-traceroute screen
+apt-get install -y git dh-autoreconf autoconf automake libtool gcc make libssl-dev libevent-dev libgeoip-dev python python-pip paris-traceroute screen
 
 # Build MeasurementKit
-cd ~/measurement-kit
+cd /home/$USER/mlab-pi/measurement-kit
 ./autogen.sh
 ./configure
 make 
@@ -11,16 +20,16 @@ make install
 ldconfig
 
 # Move the GeoIP files that MeasurementKit needs into a common folder
-mv GeoIP* ~/mlab-pi/test-runner/
+mv GeoIP* /home/$USER/mlab-pi/test-runner/
 
 # leftover from mtlynch's ndt python wrapper - needed for display?
 # Install required python libraries
-cd ~/mlab-pi/test-runner
+cd /home/$USER/mlab-pi/test-runner
 pip install pytz tzlocal
 
 # Setup data folder and starter files
-mkdir ~/mlab-pi/dashboard/data
-touch ~/mlab-pi/dashboard/data/ndt-history.csv
-echo "date,ndt_server,upload_throughput,download_throughput" >> ~/mlab-ndt/dashboard/data/ndt-history.csv
-touch ~/mlab-pi/dashboard/data/mlab-pt-paths.csv
-touch ~/mlab-pi/dashboard/data/alexa-pt-paths.csv
+mkdir /home/$USER/mlab-pi/dashboard/data
+touch /home/$USER/mlab-pi/dashboard/data/ndt-history.csv
+echo "date,ndt_server,upload_throughput,download_throughput" >> /home/$USER/mlab-ndt/dashboard/data/ndt-history.csv
+touch /home/$USER/mlab-pi/dashboard/data/mlab-pt-paths.csv
+touch /home/$USER/mlab-pi/dashboard/data/alexa-pt-paths.csv
